1fc109f6c37e7fcb3a59338f71043f2c24d2b5d2
Philip Saa <cowglow@gmail.com>
2025-02-07 15:06:56 +0100

    Add base path to generated files

--- generate-static-site.php
+++ generate-static-site.php
@@ -16,7 +16,7 @@ $kirby = kirby();
 $site = $kirby->site();
 
 // todo: fix for domains other than user.github.io, eg. custom CNAME
-$dir = '/';
+$dir = '/kirby-docker';
 if(!empty($argv[1])) {
 	$dir = $argv[1];
 }
